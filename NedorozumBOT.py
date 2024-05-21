4ee9eb2b8a18158ea2b732956a5daf3bf44e5656
Onlaperd <Onlaperd@gmail.com>
2024-05-21 21:36:54 +0300

    renamed variables

--- NedorozumBOT.py
+++ NedorozumBOT.py
@@ -6,6 +6,7 @@ import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 
+
 def RecordAudio():
     r = sr.Recognizer()
 
@@ -23,17 +24,19 @@ def RecordAudio():
         print(f"[NedorozumBOT] Помилка під час обробки вашого брєду: {e}")
         return ""
 
-def CreateAudio(TextToConvert, filename):
+
+def CreateAudio(text_to_convert, filename):
     try:
         temp_file = f"{filename}.mp3"
 
         # Check if the file already exists
         if not os.path.exists(temp_file):
-            obj = gTTS(text=TextToConvert, lang="uk", slow=False)
+            obj = gTTS(text=text_to_convert, lang="uk", slow=False)
             obj.save(temp_file)
 
     except Exception as e:
         print(f"[NedorozumBOT] Помилка під час обробки вашого брєду: {e}")
+
 
 def run():
     print("[NedorozumBOT] Привіт! Я - NedorozumBOT. Я створений щоб бісити всіх навколо")
@@ -42,96 +45,96 @@ def run():
     while True:
         print("[NedorozumBOT] ...")
         try:
-            RecordedAudio = " " + RecordAudio()
-            print(f"[You] {RecordedAudio}")
+            recorded_audio = " " + RecordAudio()
+            print(f"[You] {recorded_audio}")
 
-            HowCount = RecordedAudio.count("як ")
-            WhenCount = RecordedAudio.count("коли ")
-            ACount = RecordedAudio.count(" а ")
-            HelloCount = RecordedAudio.count("привіт")
-            HowAreYouCount = RecordedAudio.count("як справи")
-            WhyCount = RecordedAudio.count("чому ")
-            HowMuchCount = RecordedAudio.count("скільки")
+            how_count = recorded_audio.count("як ")
+            when_count = recorded_audio.count("коли ")
+            a_count = recorded_audio.count(" а ")
+            hello_count = recorded_audio.count("привіт")
+            how_are_you_count = recorded_audio.count("як справи")
+            why_count = recorded_audio.count("чому ")
+            how_much_count = recorded_audio.count("скільки")
 
-            if RecordedAudio == "":
+            if recorded_audio == "":
                 pass
-            elif HowAreYouCount > 0:
+            elif how_are_you_count > 0:
                 print("[NedorozumBOT] Ти занадто бананальний. нудно")
                 playsound("GETOUTAHERE.mp3")
                 input("[натисніть ENTER щоб закрити програму]")
                 exit()
-            elif HelloCount > 0:
+            elif hello_count > 0:
                 print("[NedorozumBOT] Привіт, чого тобі треба?")
                 playsound("HelloShort.mp3")
-            elif RecordedAudio.lower() == " слава україні":
+            elif recorded_audio.lower() == " слава україні":
                 print("[NedorozumBOT] Героям Слава!")
                 playsound("G2.mp3")
-            elif RecordedAudio.lower() == " героям слава":
+            elif recorded_audio.lower() == " героям слава":
                 print("[NedorozumBOT] Слава Нації!")
                 playsound("G3.mp3")
-            elif RecordedAudio.lower() == " слава нації":
+            elif recorded_audio.lower() == " слава нації":
                 print("[NedorozumBOT] Смерть Ворогам!")
                 playsound("G4.mp3")
-            elif RecordedAudio.lower() == " смерть ворогам":
+            elif recorded_audio.lower() == " смерть ворогам":
                 print("[NedorozumBOT] Україна!")
                 playsound("G5.mp3")
-            elif RecordedAudio.lower() == " україна":
+            elif recorded_audio.lower() == " україна":
                 print("[NedorozumBOT] Понад Усе!")
                 playsound("G6.mp3")
-            elif RecordedAudio.lower() == " понад усе":
+            elif recorded_audio.lower() == " понад усе":
                 print("[NedorozumBOT] скажи паляниця")
                 playsound("G7.mp3")
-            elif RecordedAudio.lower() == " паляниця":
+            elif recorded_audio.lower() == " паляниця":
                 print("[NedorozumBOT] молодець, не москаль")
                 playsound("G8.mp3")
-            elif HowCount > 0:
+            elif how_count > 0:
                 print("[NedorozumBOT] ніяк")
                 playsound("NoHow.mp3")
-            elif WhenCount > 0:
+            elif when_count > 0:
                 print("[NedorozumBOT] ніколи")
                 playsound("NoWhen.mp3")
-            elif WhyCount > 0:
+            elif why_count > 0:
                 print("[NedorozumBOT] тому, що")
                 playsound("Because.mp3")
-            elif HowMuchCount > 0:
+            elif how_much_count > 0:
                 print("[NedorozumBOT] ніскільки")
                 playsound("NoHowmuch.mp3")
-            elif ACount > 0:
-                WhatToSay = choice(["yes.mp3", "no.mp3", "maybe.mp3", "IDK.mp3", "GoToAss.mp3"])
-                if WhatToSay == "yes.mp3":
+            elif a_count > 0:
+                what_to_say = choice(["yes.mp3", "no.mp3", "maybe.mp3", "IDK.mp3", "GoToAss.mp3"])
+                if what_to_say == "yes.mp3":
                     print("[NedorozumBOT] так")
-                elif WhatToSay == "no.mp3":
+                elif what_to_say == "no.mp3":
                     print("[NedorozumBOT] ні")
-                elif WhatToSay == "maybe.mp3":
+                elif what_to_say == "maybe.mp3":
                     print("[NedorozumBOT] мабуть")
-                elif WhatToSay == "IDK.mp3":
+                elif what_to_say == "IDK.mp3":
                     print("[NedorozumBOT] не знаю")
-                elif WhatToSay == "GoToAss.mp3":
+                elif what_to_say == "GoToAss.mp3":
                     print("[NedorozumBOT] йди в дупу")
             else:
-                WhatToSaylist = open("PhrasesNames.txt", "r").read().split("\n")
-                ChoosedOption = randint(0, len(WhatToSaylist) - 1)
+                what_to_say_list = open("PhrasesNames.txt", "r").read().split("\n")
+                choosed_option = randint(0, len(what_to_say_list) - 1)
 
-                TextVersion = open("Phrases.txt", "r").read().split("\n")
+                text_version = open("Phrases.txt", "r").read().split("\n")
 
-                print(f"[NedorozumBOT] {TextVersion[ChoosedOption]}")
-                playsound(WhatToSaylist[ChoosedOption])
+                print(f"[NedorozumBOT] {text_version[choosed_option]}")
+                playsound(what_to_say_list[choosed_option])
 
-                Data = open("Phrases.txt", "a")
-                Data.write(RecordedAudio + "\n")
-                Data.close()
+                data = open("Phrases.txt", "a")
+                data.write(recorded_audio + "\n")
+                data.close()
 
-                Data = open("PhrasesNames.txt", "r")
-                TMP = Data.read()
-                TMP1 = TMP.split("\n")
-                HowMuchExistsAlready = len(TMP1)
-                Data.close()
+                data = open("PhrasesNames.txt", "r")
+                tmp = data.read()
+                tmp1 = tmp.split("\n")
+                how_much_exists_already = len(tmp1)
+                data.close()
 
-                Data = open("PhrasesNames.txt", "a")
-                Data.write(f"UG{HowMuchExistsAlready}.mp3\n")
-                Data.close()
+                data = open("PhrasesNames.txt", "a")
+                data.write(f"UG{how_much_exists_already}.mp3\n")
+                data.close()
 
-                CreateAudio(RecordedAudio, f"UG{HowMuchExistsAlready}")
+                CreateAudio(recorded_audio, f"UG{how_much_exists_already}")
 
         except Exception as e:
             print("[NedorozumBOT] Шо-шо? Не чує баба")
